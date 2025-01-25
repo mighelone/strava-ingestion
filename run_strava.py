bfbd654b9ff508d3b2f1ad944dc3ed5ccec24ebd
mighelone <michele.vascellari@gmail.com>
2025-01-25 15:55:40 +0000

    Use postgres (#5)

--- run_strava.py
+++ run_strava.py
@@ -6,9 +6,8 @@ pipeline = dlt.pipeline(
     import_schema_path="./schemas/import",
     export_schema_path="./schemas/export",
     pipeline_name="strava_ingestion",
-    destination="motherduck",
+    destination="postgres",
     dataset_name="strava",
-    # full_refresh=True
 )
 pipeline_info = pipeline.run(data=get_activities())
 print(pipeline.default_schema.to_pretty_yaml())
